b67364636e9a606c6e7089422f7eb119169d6a64
tsfk9981 <tsfk9981@gmail.com>
2025-03-05 22:04:38 +0100

    trying to reduce reading time

--- read_PT.m
+++ read_PT.m
@@ -7,13 +7,15 @@ points = round(time_length*FS_MODULE);
 
 Voltage = zeros(points, length(Handle_arr));
 Time = zeros(points, length(Handle_arr));
+
 for device_num = 1: length(Handle_arr)
     handle = Handle_arr(device_num);
     Raw = zeros(points, 6);
+    tic
     for i=1:points
         Raw(i,:) = fread(handle, 6, 'uint8'); % read out raw values from the device
     end
-    
+    toc
     % streamed byte array
     % 1 byte: delimiter(always 0)
     % 1 byte: timestamp (inclements up to 255 at 7812.5Hz)
@@ -21,19 +23,19 @@ for device_num = 1: length(Handle_arr)
     % 2 byte: V.R (measured right pin voltage (Volts))
 
     t = zeros(points, 1);
-    l = zeros(points, 1);
-    r = zeros(points, 1);
-
     t_init = Raw(1,2); % first number of time incrementation
     t_base = 0;
+    tic
     for i=2:points
         if(Raw(i,2)==0)
             t_base = t_base + 255; % convert 0-255 to 0-inf
         end
         t(i) = Raw(i,2) - t_init + t_base;
-        l(i) = Raw(i,3) + Raw(i,4)*255;
-        r(i) = Raw(i,5) + Raw(i,6)*255;
     end
+    l = Raw(:,3) + Raw(:,4)*255;
+    r = Raw(:,5) + Raw(:,6)*255;
+
+    toc
     Voltage(:, device_num) = l - r;
     Time(:, device_num) = t/FS_MODULE;
 
